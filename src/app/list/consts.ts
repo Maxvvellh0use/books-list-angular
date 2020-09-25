eb610a395d41c8377ad4519a1d626c673744d9db
Maksim Pavlov <pavlovmark100@gmail.com>
2020-09-26 01:16:29 +0400

    fix: change id

--- src/app/list/consts.ts
+++ src/app/list/consts.ts
@@ -1,36 +1,36 @@
 export const START_BOOKS_DATA = [
   {
-    id: 1,
+    id: '5qg34x4ve',
     title: 'War and peace',
     author: 'Leo Tolstoy',
     country: 'Russia'
   },
   {
-    id: 2,
+    id: '1l90whzh9',
     title: '1984',
     author: 'George Orwell',
     country: 'United Kingdom'
   },
   {
-    id: 3,
+    id: 'kpwfq2okw',
     title: 'Lolita',
     author: 'Vladimir Nabokov',
     country: 'France'
   },
   {
-    id: 4,
+    id: '1cqtzll2t',
     title: 'Pride and Prejudice',
     author: 'Jane Austen',
     country: 'United Kingdom'
   },
   {
-    id: 5,
+    id: '1tsbqzqoy',
     title: 'The Great Gatsby',
     author: 'F. Scott Fitzgerald',
     country: 'United States'
   },
   {
-    id: 6,
+    id: 'iznu4kq93',
     title: 'Hamlet',
     author: 'William Shakespeare',
     country: 'United Kingdom'
